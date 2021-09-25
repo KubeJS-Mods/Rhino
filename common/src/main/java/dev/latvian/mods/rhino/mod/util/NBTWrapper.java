7b0defeb6b444b0280d8da5928ba25d07b2351ea
LatvianModder <latvianmodder@gmail.com>
2021-09-25 13:53:34 +0300

    Added short versions of NBT function names

--- common/src/main/java/dev/latvian/mods/rhino/mod/util/NBTWrapper.java
+++ common/src/main/java/dev/latvian/mods/rhino/mod/util/NBTWrapper.java
@@ -59,7 +59,15 @@ public interface NBTWrapper {
 		return ByteTag.valueOf(v);
 	}
 
+	static Tag b(byte v) {
+		return ByteTag.valueOf(v);
+	}
+
 	static Tag shortTag(short v) {
+		return ShortTag.valueOf(v);
+	}
+
+	static Tag s(short v) {
 		return ShortTag.valueOf(v);
 	}
 
@@ -67,7 +75,15 @@ public interface NBTWrapper {
 		return IntTag.valueOf(v);
 	}
 
+	static Tag i(int v) {
+		return IntTag.valueOf(v);
+	}
+
 	static Tag longTag(long v) {
+		return LongTag.valueOf(v);
+	}
+
+	static Tag l(long v) {
 		return LongTag.valueOf(v);
 	}
 
@@ -75,7 +91,15 @@ public interface NBTWrapper {
 		return FloatTag.valueOf(v);
 	}
 
+	static Tag f(float v) {
+		return FloatTag.valueOf(v);
+	}
+
 	static Tag doubleTag(double v) {
+		return DoubleTag.valueOf(v);
+	}
+
+	static Tag d(double v) {
 		return DoubleTag.valueOf(v);
 	}
 
@@ -87,11 +111,23 @@ public interface NBTWrapper {
 		return new IntArrayTag(v);
 	}
 
+	static Tag ia(int[] v) {
+		return new IntArrayTag(v);
+	}
+
 	static Tag longArrayTag(long[] v) {
 		return new LongArrayTag(v);
 	}
 
+	static Tag la(long[] v) {
+		return new LongArrayTag(v);
+	}
+
 	static Tag byteArrayTag(byte[] v) {
+		return new ByteArrayTag(v);
+	}
+
+	static Tag ba(byte[] v) {
 		return new ByteArrayTag(v);
 	}
 }
