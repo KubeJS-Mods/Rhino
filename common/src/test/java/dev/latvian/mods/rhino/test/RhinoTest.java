61c036a499393e8ae09e8b9b0dc98482bbec6e08
LatvianModder <latvianmodder@gmail.com>
2021-12-24 23:38:08 +0200

    Added a simple test

---
+++ common/src/test/java/dev/latvian/mods/rhino/test/RhinoTest.java
@@ -0,0 +1,46 @@
+package dev.latvian.mods.rhino.test;
+
+import dev.latvian.mods.rhino.Context;
+import dev.latvian.mods.rhino.NativeJavaClass;
+import dev.latvian.mods.rhino.ScriptableObject;
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedInputStream;
+import java.nio.charset.StandardCharsets;
+
+public class RhinoTest {
+	public static void main(String[] args) {
+		var context = Context.enterWithNewFactory();
+		// context.setClassShutter((fullClassName, type) -> type != ClassShutter.TYPE_CLASS_IN_PACKAGE || isClassAllowed(fullClassName));
+
+		RhinoTest test = new RhinoTest(context);
+		test.add("console", new TestConsole());
+
+		test.load("/rhinotest/test.js");
+	}
+
+	public final Context context;
+	public final ScriptableObject scope;
+
+	public RhinoTest(Context c) {
+		context = c;
+		scope = context.initStandardObjects();
+	}
+
+	public void add(String name, Object value) {
+		if (value.getClass() == Class.class) {
+			ScriptableObject.putProperty(scope, name, new NativeJavaClass(scope, (Class<?>) value));
+		} else {
+			ScriptableObject.putProperty(scope, name, Context.javaToJS(value, scope));
+		}
+	}
+
+	public void load(String file) {
+		try (var stream = RhinoTest.class.getResourceAsStream(file)) {
+			var script = new String(IOUtils.toByteArray(new BufferedInputStream(stream)), StandardCharsets.UTF_8);
+			context.evaluateString(scope, script, file, 1, null);
+		} catch (Throwable ex) {
+			ex.printStackTrace();
+		}
+	}
+}
---
+++ common/src/test/java/dev/latvian/mods/rhino/test/TestConsole.java
@@ -0,0 +1,7 @@
+package dev.latvian.mods.rhino.test;
+
+public class TestConsole {
+	public void info(Object o) {
+		System.out.println(o);
+	}
+}
