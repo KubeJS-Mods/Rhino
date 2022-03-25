da4b4576ac8f79defe3f528d748e6bfc25387460
LatvianModder <latvianmodder@gmail.com>
2022-03-25 03:11:28 +0200

    Added WIP mojang mapping Remappers, Only works for Fabric for now

--- common/src/main/java/dev/latvian/mods/rhino/util/DefaultRemapper.java
+++ common/src/main/java/dev/latvian/mods/rhino/util/DefaultRemapper.java
@@ -1,7 +1,7 @@
 package dev.latvian.mods.rhino.util;
 
-import java.lang.reflect.AnnotatedElement;
-import java.lang.reflect.Member;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class DefaultRemapper implements Remapper {
 	public static final DefaultRemapper INSTANCE = new DefaultRemapper();
@@ -10,13 +10,27 @@ public class DefaultRemapper implements Remapper {
 	}
 
 	@Override
-	public String remap(Class<?> from, Member member) {
-		if (member instanceof AnnotatedElement annotatedElement) {
-			RemapForJS remap = annotatedElement.getAnnotation(RemapForJS.class);
+	public String remapClass(Class<?> from) {
+		return "";
+	}
 
-			if (remap != null) {
-				return remap.value();
-			}
+	@Override
+	public String remapField(Class<?> from, Field field) {
+		RemapForJS remap = field.getAnnotation(RemapForJS.class);
+
+		if (remap != null) {
+			return remap.value();
+		}
+
+		return "";
+	}
+
+	@Override
+	public String remapMethod(Class<?> from, Method method) {
+		RemapForJS remap = method.getAnnotation(RemapForJS.class);
+
+		if (remap != null) {
+			return remap.value();
 		}
 
 		return "";
