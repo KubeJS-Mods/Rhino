315621255d9a1574c50a89106aef3aef6097a45e
Max <maxh2709@gmail.com>
2022-06-09 22:38:01 +0200

    Use new base gradle structure, as well as ServiceLoader over @ExpectPlatform

--- common/src/main/java/dev/latvian/mods/rhino/mod/util/RemappingHelper.java
+++ common/src/main/java/dev/latvian/mods/rhino/mod/util/RemappingHelper.java
@@ -1,14 +1,23 @@
 package dev.latvian.mods.rhino.mod.util;
 
+import com.google.common.base.Suppliers;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import dev.latvian.mods.rhino.util.DefaultRemapper;
 import dev.latvian.mods.rhino.util.FallbackRemapper;
 import dev.latvian.mods.rhino.util.Remapper;
 
+import java.util.ServiceLoader;
+import java.util.function.Supplier;
+
 public class RemappingHelper {
-	@ExpectPlatform
+
+	private static final Supplier<MinecraftRemapper> MINECRAFT_REMAPPER = Suppliers.memoize(() -> {
+		var serviceLoader = ServiceLoader.load(MinecraftRemapper.class);
+		return serviceLoader.findFirst().orElseThrow(() -> new RuntimeException("Could not find a MinecraftRemapper for your platform!"));
+	});
+
 	public static MinecraftRemapper getMinecraftRemapper() {
-		throw new AssertionError();
+		return MINECRAFT_REMAPPER.get();
 	}
 
 	public static Remapper createModRemapper() {
--- forge/src/main/java/dev/latvian/mods/rhino/mod/util/forge/RemappingHelperImpl.java
+++
@@ -1,9 +0,0 @@
-package dev.latvian.mods.rhino.mod.util.forge;
-
-import dev.latvian.mods.rhino.mod.util.MinecraftRemapper;
-
-public class RemappingHelperImpl {
-	public static MinecraftRemapper getMinecraftRemapper() {
-		return ForgeRemapper.INSTANCE;
-	}
-}
