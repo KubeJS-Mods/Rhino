b8b201b9139bcc5463170d3495ad281ce9019515
ZZZank <3410764033@qq.com>
2025-02-13 13:47:58 +0800

    test++

--- src/test/java/dev/latvian/mods/rhino/test/TypeConsolidatorTest.java
+++ src/test/java/dev/latvian/mods/rhino/test/TypeConsolidatorTest.java
@@ -2,24 +2,54 @@ package dev.latvian.mods.rhino.test;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author ZZZank
  */
 public class TypeConsolidatorTest {
 	public static final RhinoTest TEST = new RhinoTest("type_consolidator");
 
-	public record Gener<T>(RhinoTest test) {
-		public static final Gener<?> NONE = new Gener<>(TEST);
+	public static final class Gener<T> {
+		public static final Gener<?> NONE = new Gener<>(TEST, List.of());
+		private final RhinoTest test;
+		private final List<T> loaded;
+		/**
+		 * test type consolidation for field
+		 */
+		public T example = null;
+
+		public Gener(RhinoTest test, List<T> loaded) {
+			this.test = test;
+			this.loaded = loaded;
+		}
 
 		/**
 		 * simulate what bindings will usually do, (also there's no other easy way of passing full type info to JS)
 		 */
-		public static Gener<TestConsoleTheme> get() {
-			return new Gener<>(TEST);
+		public static Gener<TestConsoleTheme> of() {
+			return new Gener<>(TEST, new ArrayList<>());
 		}
 
+		/**
+		 * test type consolidation for param
+		 */
 		public void load(T input) {
+			loaded.add(input);
 			test.console.info("type: %s, value: %s".formatted(input.getClass(), input));
+		}
+
+		/**
+		 * test type consolidation for return
+		 */
+		public T get(int index) {
+			return loaded.get(index);
+		}
+
+		@Override
+		public String toString() {
+			return "Gener[test=%s, loaded=%s]".formatted(test, loaded);
 		}
 	}
 
@@ -29,9 +59,18 @@ public class TypeConsolidatorTest {
 
 	@Test
 	void test() {
-		TEST.test("wrap_string", """
-			shared.Gener.get().load("dArk")
+		TEST.test("get_set", """
+			const g = shared.Gener.of();
+			g.load("dArk");
+			console.info(g.get(0));
+			g.example = "light"
+			console.info(g.example);
 			""", """
-			type: %s, value: %s""".formatted(TestConsoleTheme.class, TestConsoleTheme.DARK));
+			type: %s, value: %s
+			%s
+			%s""".formatted(
+			TestConsoleTheme.class, TestConsoleTheme.DARK,
+			TestConsoleTheme.DARK,
+			TestConsoleTheme.LIGHT));
 	}
 }
